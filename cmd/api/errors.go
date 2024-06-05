d23d5bc10c2541b4466659e6888cfd6e8b14df0e
Yousif Sabah <iyousifsabah@gmail.com>
2024-06-05 23:27:38 +0300

    cmd/api: fix bad request status

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -37,7 +37,7 @@ func (app *application) methodNotAllowed(w http.ResponseWriter, r *http.Request)
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.errorResponse(w, r, http.StatusMethodNotAllowed, err.Error())
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
